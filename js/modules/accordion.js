7a598c7ec10c4201185a331f55796eb817c58fa0
andreavenue <dev.marto87@gmail.com>
2025-03-10 12:22:03 -0300

    Accordion transformado em Class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,28 @@
-export default function initAccordion(){
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-  if (accordionList.length){
-    function activeAccordion(){
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+export default class Accordion{
+  constructor(list){
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  toggleAccordion(item){
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  //Add os eventos ao accordion
+  addAccordionEvent(){
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
+
+  // Iniciar função
+  init(){
+    if (this.accordionList.length) {
+      //Ativar primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+  }
+
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import SmoothScroll from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import initAnimateScroll from './modules/scroll-animate.js';
-import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab.js';
 import initModal from './modules/modal.js';
 import iniTooltip from './modules/tooltip.js';
@@ -13,9 +13,12 @@ import initFecthBitcoin from './modules/fetch-bitcoin.js';
 const smoothScroll = new SmoothScroll('[data-menu="suave"] a[href^="#"]');
 smoothScroll.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+
 initTabNav();
 initAnimateScroll();
-initAccordion();
 initModal();
 iniTooltip();
 initDropDownMenu();
