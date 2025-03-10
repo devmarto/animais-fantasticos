2236660fc8d058ddab93133974ebe86bab70f8cd
andreavenue <dev.marto87@gmail.com>
2025-03-10 11:51:35 -0300

    SmoothScroll transformado em Classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,18 +1,36 @@
-export default function initSmoothScroll(){
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-  function scrollToSection(event){
+export default class SmoothScroll{
+
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined){
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event){
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+  addLinkEvent(){
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
     });
-  };
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  }
+  init(){
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
 
-//initSmoothScroll();
+
