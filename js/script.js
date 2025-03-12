3649a5e850612e57e18e1438bb387bd8f800ed13
andreavenue <dev.marto87@gmail.com>
2025-03-12 17:20:18 -0300

    Adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
+import SlideNav from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -31,3 +32,8 @@ initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
 initAnimacaoScroll();
+
+const slide = new SlideNav( '.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
+
